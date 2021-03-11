8847bf47d9a17da997c6d962cc570f3ca767e1ae
Paul Nicholsen <paulnicholsen27@gmail.com>
2021-03-11 17:27:13 -0500

    added password helper text and put back user routes

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # To do
 
 - edit password / make new user
-- phone number validation
-- strip punctuation from phone numbers on save
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#login_form"
   post "/login", to: "sessions#login"
   get "/logout", to: "sessions#logout"
-  # resources :users, only: [:new, :create, :edit] 
+  resources :users, only: [:new, :create, :edit, :update] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
