e5137a7280f177d35fc1c5a9a9d45f9c9a06be98
Paul Nicholsen <paulnicholsen27@gmail.com>
2021-03-11 17:06:16 -0500

    added phonelib library and implemented form validations for new singers

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,2 +1,10 @@
 class Singer < ApplicationRecord
+
+        validates :first_name, presence: true
+        validates :last_name, presence: true
+        validates :phone_number, presence: true, phone: true
+
+        def phone_number=(str) 
+            @phone_number = str.tr('^0-9', '')
+        end
 end
