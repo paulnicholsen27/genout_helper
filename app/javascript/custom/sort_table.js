dea0caacc078e81b4fb1475605a7904bbd57be6a
Paul Nicholsen <paulnicholsen27@gmail.com>
2021-06-22 20:45:00 -0400

    can sort member table

--- app/javascript/custom/sort_table.js
+++ app/javascript/custom/sort_table.js
@@ -1,46 +1,60 @@
-//  sortTable(f,n)
-//  f : 1 ascending order, -1 descending order
-//  n : n-th child(<td>) of <tr>
-
-function sortTable(f, n) {
-    var rows = $('#sortable tbody tr').get();
-
-    rows.sort(function (a, b) {
-
-        var A = getVal(a);
-        var B = getVal(b);
-
-        if (A < B) {
-            return -1 * f;
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("sortable_table");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
         }
-        if (A > B) {
-            return 1 * f;
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
         }
-        return 0;
-    });
-
-    function getVal(elm) {
-        var v = $(elm).children('td').eq(n).text().toUpperCase();
-        if ($.isNumeric(v)) {
-            v = parseInt(v, 10);
-        }
-        return v;
+      }
     }
-
-    $.each(rows, function (index, row) {
-        $('#sortable').children('tbody').append(row);
-    });
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
-var f_sl = 1; // flag to toggle the sorting order
-var f_nm = 1; // flag to toggle the sorting order
-$("#sl").on("click", function () {
-    console.log("sl clicked")
-    f_sl *= -1; // toggle the sorting order
-    var n = $(this).prevAll().length;
-    sortTable(f_sl, n);
-});
-$("#nm").click(function () {
-    f_nm *= -1; // toggle the sorting order
-    var n = $(this).prevAll().length;
-    sortTable(f_nm, n);
-});
+
+$(document).ready(function(){
+    $(".sortable").click(function(){
+        sortTable($(this).parent().children().index($(this)))
+    })
+})
